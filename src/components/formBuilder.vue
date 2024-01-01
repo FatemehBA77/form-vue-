327578e096abbb72b5bf8e04eee89d65deb761f7
seyedalihosseini <122220669+seyedalihosseiniFrontDeveloper@users.noreply.github.com>
2024-01-01 14:05:06 +0330

    add id to reset state

--- src/components/formBuilder.vue
+++ src/components/formBuilder.vue
@@ -88,6 +88,7 @@ export default {
     },
     resetformefields() {
       this.formBuild = {
+        id: Math.floor(Math.random() * 100),
         userName: "",
         password: "",
         email: "",
